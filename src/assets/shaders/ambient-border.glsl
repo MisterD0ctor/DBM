9167c4797ef9cb9286267fb14f46552668159a61
MisterD0ctor <kasper.axelsson1@gmail.com>
2026-04-20 15:10:34 +0200

    feat: increase grain amount and refactor blur functions for improved performance

--- src/assets/shaders/ambient-border.glsl
+++ src/assets/shaders/ambient-border.glsl
@@ -54,8 +54,7 @@ vec4 hook() {
 //!DESC ambient edge extension pass2
 
 #define EDGE_BLUR 0.003
-#define BORDER_BLUR 0.2
-#define EDGE_SPREAD 0.5
+#define BORDER_SPREAD 0.5
 
 #define SIGMA 0.4 //blur spread or amount, (0.0, 10+]
 #define get_weight(x) (exp(-(x) * (x) / (2.0 * SIGMA * SIGMA)))
@@ -64,7 +63,7 @@ vec4 hook() {
 #define FALLOFF_SOFTNESS 0.2 // (0.0, 1.0)
 #define light_falloff(x) (1 / (x * x + 2 * abs(x) + 1))
 
-#define GRAIN 128
+#define GRAIN 256 // grain amount, [0, 1024+]
 
 float soft_light_falloff(float x) {
     float c = (1 - FALLOFF_SOFTNESS) / FALLOFF_SOFTNESS;
@@ -73,28 +72,12 @@ float soft_light_falloff(float x) {
     return 1 / (den * den);
 }
 
-// // Wide usage friendly PRNG, shamelessly stolen from a GLSL tricks forum post
-// float mod289(float x)  { return x - floor(x / 289.0) * 289.0; }
-// float permute(float x) { return mod289((34.0*x + 1.0) * x); }
-// float rand(float x)    { return fract(x / 41.0); }
+// Wide usage friendly PRNG, shamelessly stolen from a GLSL tricks forum post
+float mod289(float x)  { return x - floor(x / 289.0) * 289.0; }
+float permute(float x) { return mod289((34.0*x + 1.0) * x); }
+float rand(float x)    { return fract(x / 41.0); }
 
-vec4 blur(sampler2D image, vec2 pos, float edge_dist, vec2 dir) {
-    float sigma = EDGE_BLUR + edge_dist * BORDER_BLUR;
-    float radius = length(sigma * 3 * HOOKED_size);
-
-    float weight;
-    vec4 csum = textureLod(image, pos, 0.0);
-    float wsum = 1.0;
-    for(float i = 1.0; i <= radius; ++i) {
-        weight = get_weight(i / radius);
-        csum += (textureLod(image, pos - i * dir * HOOKED_pt, 0.0) 
-               + textureLod(image, pos + i * dir * HOOKED_pt, 0.0)) * weight;
-        wsum += 2.0 * weight;
-    }
-    return csum / wsum;
-}
-
-vec4 blur2(sampler2D image, vec2 pos, float edge_dist, vec2 dir) {
+vec4 light_spread(sampler2D image, vec2 pos, float edge_dist, vec2 dir) {
     if (edge_dist == 0.0) return textureLod(image, pos, 0.0);
     
     float pt = length(HOOKED_pt * dir);
@@ -105,7 +88,7 @@ vec4 blur2(sampler2D image, vec2 pos, float edge_dist, vec2 dir) {
     float wsum = 0.0;
     for(float i = 0; i <= 1; i += 2 * pt) {
         float weight = light_falloff(length(vec2((i - center), edge_dist)) * FALLOFF) 
-                       * edge_dist / length(vec2((i - center), (edge_dist + EDGE_BLUR) * EDGE_SPREAD));
+                       * edge_dist / length(vec2((i - center), (edge_dist + EDGE_BLUR) * BORDER_SPREAD));
         weight = pow(weight, 2.2);
         csum += textureLod(image, pos * dir.yx + i * dir, 0.0) * weight;
         wsum += weight;
@@ -127,19 +110,19 @@ vec4 hook() {
 
     vec2 dir = abs(normalize(delta.yx));
 
-    // // Initialize the PRNG by hashing the position + a random uniform
-    // vec3 m = vec3(pos, 1.0) + vec3(textureLod(HOOKED_raw, pos, 0.0));
-    // float h = permute(permute(permute(m.x) + m.y) + m.z);
+    // Initialize the PRNG by hashing the position + the pixel color
+    vec3 m = vec3(pos, 1.0) + vec3(textureLod(HOOKED_raw, pos, 0.0));
+    float h = permute(permute(permute(m.x) + m.y) + m.z);
 
-    // // Add some random noise to the output
-    // vec4 noise;
-    // noise.x = rand(h); h = permute(h);
-    // noise.y = rand(h); h = permute(h);
-    // noise.z = rand(h); h = permute(h);
-    // noise.w = 0.5;
+    // Add some random noise to the output
+    vec4 noise;
+    noise.x = rand(h); h = permute(h);
+    noise.y = rand(h); h = permute(h);
+    noise.z = rand(h); h = permute(h);
+    noise.w = 0.5;
   
-    return blur2(HOOKED_raw, pos, dist, dir)
-           * soft_light_falloff(dist * FALLOFF);
-        //    + (GRAIN/8192.0) * (noise - vec4(0.5));
+    return light_spread(HOOKED_raw, pos, dist, dir)
+           * soft_light_falloff(dist * FALLOFF)
+           + (GRAIN/8192.0) * (noise - vec4(0.5));
 }
 
